f68e87ae2bfe801ec15fe046234a8f4917db2296
Galen O'Neil <galen.oneil@nist.gov>
2015-04-07 12:45:07 -0600

    histogram to own file with tests

--- src/steps.jl
+++ src/steps.jl
@@ -3,31 +3,7 @@ import JLD: JldGroup, JldFile, JldDataset, HDF5Dataset
 
 const DONETHRU_MAX = typemax(Int)-1
 
-type Histogram
-	x::FloatRange{Float64}
-	counts::Vector{Int}
-	seen::Int # total events seen, including those not in counts
-	counted::Int # running sum of counts
-end
-Histogram(x::Range) = Histogram(convert(FloatRange{Float64},x),zeros(Int, length(x)-1),0,0)
-bin_edges(h::Histogram) = h.x
-bin_centers(h::Histogram) = convert(FloatRange{Float64},range(first(h.x)*step(h.x)/2,step(h.x),length(h.x)-1))
-binsize(h::Histogram) = step(h.x)
-function addcounts!(h::Histogram, events::Array) 
-	newcounts = hist(events,bin_edges(h))[2]
-	h.counts+=newcounts
-	h.counted+=sum(newcounts)
-	h.seen+=length(events)
-end
-addseen!(h::Histogram,n)=h.seen+=n
-counts(h::Histogram) = h.counts
-counted(h::Histogram) = h.counted
-misses(h::Histogram) = h.seen-counted(h)
-donethru(h::Histogram) = h.seen
-function update_histogram(h::Histogram, selection, x)
-	addcounts!(h,x[selection])
-	addseen!(h,length(selection)-sum(selection))
-end
+include("histogram.jl")
 
 
 typealias Channel Dict{Symbol,Any}
